e53cd3fbb128312852eb9c3cc6e607f4847a82a7
EmptyMan0 <miha_009@bk.ru>
2022-11-14 21:27:39 +1000

    first commit

---
+++ ClassLibrary/Figures.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Figures
+    {
+        private double a, b, c, r;
+        string er = "";
+
+        public Figures(double r)//круг
+        {
+            if (r > 0)
+                this.r = r;
+            else throw new ArgumentException("Значения должны быть строго больше 0");
+        }
+        public Figures(double a, double b, double c)//треугольник
+        {
+            if (a > 0 && b > 0 && c > 0)
+            {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+
+            }//throw new Exception("Значения должны быть строго больше 0")
+            else throw new ArgumentException("Значения должны быть строго больше 0");
+        }
+        //расчёт пл круга
+        public dynamic area_of_a_circle() => Math.Round(Math.PI * Math.Pow(r, 2), 4);
+
+        //расчёт пл треугольника
+        public dynamic area_of_a_triangle()
+        {
+            var p = (a + b + c) / (2); //поиск полупериметра
+            var S = Math.Sqrt(p * (p - a) * (p - b) * (p - c)); //нахождение  площади треугольника
+            return S;
+        }
+
+    }
+}
---
+++ LibFigures/Program.cs
@@ -0,0 +1,64 @@
+﻿using ClassLibrary;
+using System;
+
+namespace LibFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pm = new Program();
+            pm.check_input();
+        }
+        dynamic check_input()
+        {
+            try
+            {
+                Console.WriteLine("Выбирете фигуру: 1.Круг. 2.Треугльник");
+                string type_figure = Console.ReadLine();
+                switch (type_figure)
+                {
+                    case "1":
+                        Console.Write("Введите радиус круга в см для вычисления площади r = ");
+                        var r = Convert.ToDouble(Console.ReadLine());
+
+                        Figures radius = new Figures(r);
+                        var rez = radius.area_of_a_circle();
+
+                        Console.WriteLine($"Площадь круга {rez} кв.см");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Введите 3 стороны треугольника(a, b, c) через пробел в см, для вычисления площади");
+                        string line = Console.ReadLine();
+                        string[] splitString = line.Split(' ');
+
+                        var a = double.Parse(splitString[0]);
+                        var b = double.Parse(splitString[1]);
+                        var c = double.Parse(splitString[2]);
+                        var max = Math.Max(c, Math.Max(a, b));
+                        var sum_perem = a + b + c;
+                        //if (sum_perem -max)
+                        //{
+                        //    string pprt = "Наибольшая сторона треугольника должна быть меньше суммы других сторон";
+                        //}
+                        string ife = (c * c == a * a + b * b) ? "Введённый Вами треугольник равнобедренный"
+                                                                : "Введённый Вами треугольник равносторонний";
+                        Console.WriteLine($"{ife}");
+                        Figures sides = new Figures(a, b, c);
+                        Console.WriteLine($"Площадь треугольника равна {sides.area_of_a_triangle()} кв. см");
+                        break;
+
+                    default:
+                        Console.Write("Такой фигуры нет в списке");
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Неверный ввод данных");
+            }
+            return 0;
+        }
+    }
+}
